5223d44a5498bf6a775fa39866fb76a2f365b7a3
MatheusWilgen <50850472+MatheusWilgen@users.noreply.github.com>
2020-11-09 21:42:30 -0300

    Jogo funcionando, falta a rede neural

--- jogo.py
+++ jogo.py
@@ -11,13 +11,15 @@ font = pygame.font.Font('freesansbold.ttf', 12)
 #asteroide
 Asteroide = [1, 1]
 Asteroide_lataria = pygame.image.load('Imagens/Asteroide.png')
-Asteroide_vel = 2
+Asteroide_vel = 5
+Asteroide_mask = pygame.mask.from_surface(Asteroide_lataria)
 
 #nave
 Nave = [300, 450]
 Nave_lataria = pygame.image.load('Imagens/Nave_Padrao.png')
 Nave_vel = 4
 score = 0
+Nave_mask = pygame.mask.from_surface(Nave_lataria)
 
 
 screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -47,7 +49,12 @@ def Nave_movimenta():
         Nave[0] -= Nave_vel
     if direcao[pygame.K_RIGHT]:
         Nave[0] += Nave_vel
-    
+
+def colisao():
+    distancia_x = Asteroide[0] - Nave[0]
+    distancia_y = Asteroide[1] - Nave[1]
+    return Nave_mask.overlap(Asteroide_mask , (distancia_x , distancia_y)) != None
+
 #Função responsável por atualizar os objetos na tela
 def atualiza_tela():
     screen.fill((0, 0, 0)) #tela preta, esta linha sempre em primeiro
@@ -67,8 +74,25 @@ while True:
             pygame.quit()
 
     score = (pygame.time.get_ticks()/1000) * 7.66 #7.66 é uma velocidade média em km/s da Estação Espacial Internacional
-
+    if(colisao()):
+        break
     Nave_movimenta()
     limites_nave()
     asteroide()
     atualiza_tela() #Deixar sempre em último
+
+#tela final
+r = g = b =0
+while True:
+    game_over_font = pygame.font.Font('freesansbold.ttf', 75)
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    game_over_screen = game_over_font.render('Game Over', True, (r, g, b))
+    screen.blit(game_over_screen, (80 , (SCREEN_SIZE[1] / 2 ) - 75))
+    pygame.display.update()
+    pygame.time.wait(200)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
