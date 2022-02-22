cf291d8eba6bed94c3aacd9ac2e51ca6b1fd2b4e
MatheusWilgen <matheuswilgengoncalves@gmail.com>
2022-02-22 19:05:35 -0300

    Upload arquivos para arquivar

--- Rede_Neural.py
+++ Rede_Neural.py
@@ -17,32 +17,15 @@ import pickle
 
 # Criando a rede Neural
 rede = buildNetwork(3, 5, 1)
-'''
-print(rede['in'])
-print(rede['hidden0'])  #Mostra que a camada oculta possui a função sigmoide
-print(rede['out'])
-print(rede['bias'])
-'''
+
 # Base de dados, dois atributos previsores e uma classe
 base = SupervisedDataSet(3, 1)
-# Adicionando os elementos(XOR)
-'''
-base.addSample((0, 0), (0,))  # Resultado 0
-base.addSample((0, 1), (1,))  # Resultado 1
-base.addSample((1, 0), (1,))  # Resultado 1
-base.addSample((1, 1), (-1,))  # Resultado 0
-'''
 
 #Incluindo o dataset do jogo
-"""
-Array_X = [0,0,1,1]
-Array_Y = [0,1,0,1]
-Array_Saida = [0, 1, 1, -1]
-"""
-Posicao_nave_X = np.loadtxt("Dataset_Posicao_X.txt")
-Array_X = np.loadtxt("Dataset_Delta_X.txt")
-Array_Y = np.loadtxt("Dataset_Delta_Y.txt")
-Array_Saida = np.loadtxt("Dataset_Y.txt")
+Posicao_nave_X    = np.loadtxt("Dataset_Posicao_X.txt")
+Array_X           = np.loadtxt("Dataset_Delta_X.txt")
+Array_Y           = np.loadtxt("Dataset_Delta_Y.txt")
+Array_Saida       = np.loadtxt("Dataset_Y.txt")
 
 for i in range(len(Array_X)):
    base.addSample((Posicao_nave_X[i] , Array_X[i] , Array_Y[i]) , (Array_Saida[i],))
@@ -54,14 +37,14 @@ print(base['target'])
 treinamento = BackpropTrainer(rede, dataset=base, learningrate=0.01, momentum = 0.01)
 
 # Controlado o número de épocas(30000 épocas)
-for i in range(1, 30000):
+for i in range(1, 1000):
    erro = treinamento.train()
    # Printando o erro e limitando a quantidade de vezes que o mesmo vai ser printado para melhor visualização. Nesse caso, será printado de 1000 em 1000
    if i % 100 == 0:
       print("Erro: %s" % erro)
 
 
-arquivo = open('arquivo.p1', 'wb')
+arquivo = open('arquivo.p', 'wb')
 pickle.dump(rede, arquivo)
 arquivo.close()
 
